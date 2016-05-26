7fde7dee11f58254ef5f14a0b365f66f8096ac2a
StreatCodes <matt.r.streatfield@gmail.com>
2016-05-26 21:30:05 +1000

    Goofed previous commit

--- js/movieSearch.js
+++ js/movieSearch.js
@@ -2,5 +2,5 @@ ReactDOM.render(React.createElement(
     'h1',
     null,
     'Hello, world!'
-), document.getElementById('example'));
+), document.getElementById('movie-search'));
 //# sourceMappingURL=C:\Users\mattr\websites\ReactBootExample\js\movieSearch.js.map
