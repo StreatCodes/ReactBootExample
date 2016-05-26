5b0c4ef2cc27df9767e4f11fd845f887fbf5838b
StreatCodes <matt.r.streatfield@gmail.com>
2016-05-26 21:25:28 +1000

    Setup react.js, starting to convert search-bar and results to react.

---
+++ js/movieSearch.js
@@ -0,0 +1,6 @@
+ReactDOM.render(React.createElement(
+    'h1',
+    null,
+    'Hello, world!'
+), document.getElementById('example'));
+//# sourceMappingURL=C:\Users\mattr\websites\ReactBootExample\js\movieSearch.js.map
