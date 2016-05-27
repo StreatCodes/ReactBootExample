6033e52825274a0db3325e5a041c00fedd6bb8eb
StreatCodes <matt.r.streatfield@gmail.com>
2016-05-27 15:09:32 +1000

    Added todo.txt, started search bar stuff

--- js/movieSearch.js
+++ js/movieSearch.js
@@ -1,69 +1,97 @@
 var SearchBar = React.createClass({
-    displayName: "SearchBar",
+    displayName: 'SearchBar',
 
+    getInitialState: function () {
+        return { inputValue: '', searchValue: '' };
+    },
+    handleChange: function (event) {
+        this.setState({ inputValue: event.target.value.substr(0, 140) });
+        clearTimeout(this.searchTimeOut);
+        this.searchTimeOut = setTimeout(function () {
+            this.setState({ searchValue: this.state.inputValue });console.log(this.state.searchValue);
+        }.bind(this), 300);
+    },
+    searchTimeOut: setTimeout(function () {}),
     render: function () {
         return React.createElement(
-            "div",
-            { "class": "col-lg-10 col-md-11 col-sm-12" },
+            'div',
+            { className: 'col-lg-10 col-md-11 col-sm-12' },
             React.createElement(
-                "form",
+                'form',
                 null,
                 React.createElement(
-                    "fieldset",
-                    { className: "form-group" },
-                    React.createElement("input", { type: "search", className: "form-control", id: "moveSearch", placeholder: "Search for a movie, tv show or actor" })
+                    'fieldset',
+                    { className: 'form-group' },
+                    React.createElement('input', {
+                        type: 'search',
+                        value: this.state.inputValue,
+                        onChange: this.handleChange,
+                        className: 'form-control', placeholder: 'Search for a movie, tv show or actor' })
                 )
             ),
-            React.createElement(SearchResults, null)
+            React.createElement(SearchResults, { searchValue: this.state.searchValue })
         );
     }
 });
 
 var SearchResults = React.createClass({
-    displayName: "SearchResults",
+    displayName: 'SearchResults',
 
     render: function () {
-        return React.createElement(
-            "div",
-            { className: "row" },
-            React.createElement(MovieCard, null),
-            React.createElement(MovieCard, null),
-            React.createElement(MovieCard, null)
-        );
+        if (this.props.searchValue === "") {
+            return React.createElement(
+                'div',
+                { style: { textAlign: 'center', color: 'rgb(180, 180, 180)' } },
+                React.createElement(
+                    'h3',
+                    null,
+                    'Waiting for search input.'
+                )
+            );
+        } else {
+            return React.createElement(
+                'div',
+                { className: 'row' },
+                React.createElement(MovieCard, null),
+                React.createElement(MovieCard, null),
+                React.createElement(MovieCard, null),
+                React.createElement(MovieCard, null)
+            );
+        }
     }
 });
 
 var MovieCard = React.createClass({
-    displayName: "MovieCard",
+    displayName: 'MovieCard',
 
     render: function () {
         return React.createElement(
-            "div",
-            { className: "col-lg-4 col-md-6 col-sm-12" },
+            'div',
+            { className: 'col-lg-4 col-md-6 col-sm-12' },
             React.createElement(
-                "div",
-                { className: "card" },
-                React.createElement("img", { className: "card-img-top", "data-src": "...", alt: "Card image cap" }),
+                'div',
+                { className: 'card' },
+                React.createElement('img', { className: 'card-img-top', 'data-src': '...', alt: 'Card image cap' }),
                 React.createElement(
-                    "div",
-                    { className: "card-block" },
+                    'div',
+                    { className: 'card-block' },
                     React.createElement(
-                        "h4",
-                        { className: "card-title" },
-                        "Card title"
+                        'h4',
+                        { className: 'card-title' },
+                        'Card title'
                     ),
                     React.createElement(
-                        "p",
-                        { className: "card-text" },
-                        "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+                        'p',
+                        { className: 'card-text' },
+                        'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
                     ),
                     React.createElement(
-                        "p",
-                        { className: "card-text" },
+                        'p',
+                        { className: 'card-text' },
                         React.createElement(
-                            "small",
-                            { "class": "text-muted" },
-                            "Last updated 3 mins ago"
+                            'small',
+                            { 'class': 'text-muted' },
+                            'Last updated 3 mins ago'
                         )
                     )
                 )
--- js/movieSearch.jsx
+++ js/movieSearch.jsx
@@ -1,43 +1,64 @@
 var SearchBar = React.createClass({
-  render: function() {
+    getInitialState: function() {
+        return {inputValue: '', searchValue: ''};
+    },
+    handleChange: function(event) {
+        this.setState({inputValue: event.target.value.substr(0, 140)});
+        clearTimeout(this.searchTimeOut);
+        this.searchTimeOut = setTimeout(function(){ this.setState({searchValue: this.state.inputValue}); console.log(this.state.searchValue)}.bind(this), 300)
+    },
+    searchTimeOut: setTimeout(function(){}),
+    render: function() {
     return (
-        <div class="col-lg-10 col-md-11 col-sm-12">
+        <div className="col-lg-10 col-md-11 col-sm-12">
             <form>
                 <fieldset className="form-group">
-                    <input type="search" className="form-control" id="moveSearch" placeholder="Search for a movie, tv show or actor" />
+                    <input
+                        type="search"
+                        value={this.state.inputValue}
+                        onChange={this.handleChange}
+                        className="form-control" placeholder="Search for a movie, tv show or actor" />
                 </fieldset>
             </form>
-            <SearchResults />
+            <SearchResults searchValue={this.state.searchValue}/>
         </div>
         );
     }
 });
 
 var SearchResults = React.createClass({
-  render: function() {
-    return (
-        <div className="row">
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-        </div>
-        );
+    render: function() {
+        if(this.props.searchValue === ""){
+            return (
+                <div style={{textAlign: 'center', color: 'rgb(180, 180, 180)'}}>
+                    <h3>Waiting for search input.</h3>
+                </div>
+            );}
+        else {
+            return (
+            <div className="row">
+                <MovieCard />
+                <MovieCard />
+                <MovieCard />
+                <MovieCard />
+            </div>
+        );}
     }
 });
 
 var MovieCard = React.createClass({
-  render: function() {
-    return (
-        <div className="col-lg-4 col-md-6 col-sm-12">
-            <div className="card">
-                <img className="card-img-top" data-src="..." alt="Card image cap" />
-                <div className="card-block">
-                    <h4 className="card-title">Card title</h4>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    render: function() {
+        return (
+            <div className="col-lg-4 col-md-6 col-sm-12">
+                <div className="card">
+                    <img className="card-img-top" data-src="..." alt="Card image cap" />
+                    <div className="card-block">
+                        <h4 className="card-title">Card title</h4>
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
                 </div>
             </div>
-        </div>
         );
     }
 });
