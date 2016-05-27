26d45a9a1072593bb5da5ad9a81c084f258e2c19
StreatCodes <matt.r.streatfield@gmail.com>
2016-05-27 12:16:32 +1000

    Further converted page setup to ReactJS

--- js/movieSearch.js
+++ js/movieSearch.js
@@ -1,6 +1,76 @@
-ReactDOM.render(React.createElement(
-    'h1',
-    null,
-    'Hello, world!'
-), document.getElementById('movie-search'));
+var SearchBar = React.createClass({
+    displayName: "SearchBar",
+
+    render: function () {
+        return React.createElement(
+            "div",
+            { "class": "col-lg-10 col-md-11 col-sm-12" },
+            React.createElement(
+                "form",
+                null,
+                React.createElement(
+                    "fieldset",
+                    { className: "form-group" },
+                    React.createElement("input", { type: "search", className: "form-control", id: "moveSearch", placeholder: "Search for a movie, tv show or actor" })
+                )
+            ),
+            React.createElement(SearchResults, null)
+        );
+    }
+});
+
+var SearchResults = React.createClass({
+    displayName: "SearchResults",
+
+    render: function () {
+        return React.createElement(
+            "div",
+            { className: "row" },
+            React.createElement(MovieCard, null),
+            React.createElement(MovieCard, null),
+            React.createElement(MovieCard, null)
+        );
+    }
+});
+
+var MovieCard = React.createClass({
+    displayName: "MovieCard",
+
+    render: function () {
+        return React.createElement(
+            "div",
+            { className: "col-lg-4 col-md-6 col-sm-12" },
+            React.createElement(
+                "div",
+                { className: "card" },
+                React.createElement("img", { className: "card-img-top", "data-src": "...", alt: "Card image cap" }),
+                React.createElement(
+                    "div",
+                    { className: "card-block" },
+                    React.createElement(
+                        "h4",
+                        { className: "card-title" },
+                        "Card title"
+                    ),
+                    React.createElement(
+                        "p",
+                        { className: "card-text" },
+                        "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+                    ),
+                    React.createElement(
+                        "p",
+                        { className: "card-text" },
+                        React.createElement(
+                            "small",
+                            { "class": "text-muted" },
+                            "Last updated 3 mins ago"
+                        )
+                    )
+                )
+            )
+        );
+    }
+});
+
+ReactDOM.render(React.createElement(SearchBar, null), document.getElementById('movie-search'));
 //# sourceMappingURL=C:\Users\mattr\websites\ReactBootExample\js\movieSearch.js.map
--- js/movieSearch.jsx
+++ js/movieSearch.jsx
@@ -1,3 +1,49 @@
+var SearchBar = React.createClass({
+  render: function() {
+    return (
+        <div class="col-lg-10 col-md-11 col-sm-12">
+            <form>
+                <fieldset className="form-group">
+                    <input type="search" className="form-control" id="moveSearch" placeholder="Search for a movie, tv show or actor" />
+                </fieldset>
+            </form>
+            <SearchResults />
+        </div>
+        );
+    }
+});
+
+var SearchResults = React.createClass({
+  render: function() {
+    return (
+        <div className="row">
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+        </div>
+        );
+    }
+});
+
+var MovieCard = React.createClass({
+  render: function() {
+    return (
+        <div className="col-lg-4 col-md-6 col-sm-12">
+            <div className="card">
+                <img className="card-img-top" data-src="..." alt="Card image cap" />
+                <div className="card-block">
+                    <h4 className="card-title">Card title</h4>
+                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+        );
+    }
+});
+
+
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>, document.getElementById('movie-search')
+    <SearchBar />, document.getElementById('movie-search')
 );
