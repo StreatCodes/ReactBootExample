4f79aedcb5fe1ba3bf82f79a6b3bb555581b413c
StreatCodes <matt.r.streatfield@gmail.com>
2016-06-02 00:39:06 +1000

    Yikes, huge changes while hunting down a bug, don't use onClick recklessly, pages now work perfectly as far as i can tell.

--- js/movieSearch.js
+++ js/movieSearch.js
@@ -2,7 +2,7 @@ var SearchBar = React.createClass({
     displayName: 'SearchBar',
 
     getInitialState: function () {
-        return { inputValue: '', searchValue: '', selectValue: 'All' };
+        return { inputValue: '', searchValue: '', selectValue: 'multi' };
     },
     handleChange: function (event) {
         //Cut user input to max length of 140 characters.
@@ -11,13 +11,36 @@ var SearchBar = React.createClass({
         this.setState({ inputValue: event.target.value.substr(0, 140) });
         clearTimeout(this.searchTimeOut);
         this.searchTimeOut = setTimeout(function () {
-            this.setState({ searchValue: this.state.inputValue });
+            //only set the state if the input has changed and isn't empty.
+            if (this.state.inputValue !== this.state.searchValue && this.state.inputValue !== "") {
+                this.setState({ searchValue: this.state.inputValue });
+            }
         }.bind(this), 300);
     },
     handleSelect: function (event) {
         //Handles search bar dropdown menu.
         if (this.state.selectValue !== event.target.innerHTML) {
-            this.setState({ selectValue: event.target.innerHTML });
+            var type;
+
+            //Set prop to appropriate string for the search query later.
+            switch (event.target.innerHTML) {
+                case 'All':
+                    type = "multi";
+                    break;
+                case 'Movie':
+                    type = "movie";
+                    break;
+                case 'TV Show':
+                    type = "tv";
+                    break;
+                case 'Actors':
+                    type = "person";
+                    break;
+                default:
+                    type = "multi";
+                    console.log('Error this should never happen :(');
+            }
+            this.setState({ selectValue: type });
         }
     },
     searchTimeOut: setTimeout(function () {}),
@@ -78,50 +101,45 @@ var SearchResults = React.createClass({
     displayName: 'SearchResults',
 
     getInitialState: function () {
-        return { searchResults: [], loading: false, searchQuery: "EMPTY" };
+        return { searchResults: [], loading: false, searchQuery: "EMPTY", selectValue: "EMPTY", totalPages: 0, currentPage: 1 };
+    },
+    handlePageSelect: function (page, event) {
+        //Handle pagination clicks and sets page to new one
+        if (page !== this.state.currentPage) {
+            this.setState({ currentPage: page, loading: true });
+            console.log('PAGE SELECTION NUMBER: ' + page);
+        }
+    },
+    apiQuery: function (nextState) {
+        //Setup url for search
+        var baseURL = "http://api.themoviedb.org/3/search/";
+        var type = nextState.selectValue;
+        var options = "?api_key=1fa14e6c92287ef10dc7a09da28b8f28&language=en&query=";
+        var page = "&page=" + nextState.currentPage;
+
+        var query = baseURL + type + options + nextState.searchQuery + page;
+        console.log('search query: ' + query);
+        //Submit search query to The Movie Database servers
+        $.getJSON(query, function (data) {
+            this.setState({ loading: false, searchResults: data, totalPages: data.total_pages, currentPage: data.page });
+        }.bind(this));
     },
     componentWillReceiveProps: function (nextProps) {
-        //If search value changed send movie api request and set loading to true.
-        if (nextProps.searchValue !== "" && (nextProps.searchValue !== this.props.searchValue || this.props.selectValue !== nextProps.selectValue)) {
-            this.setState({ loading: true, searchQuery: nextProps.searchValue });
-
-            //Setup url for search
-            var baseURL = "http://api.themoviedb.org/3/search/";
-            var type = "";
-            var options = "?api_key=1fa14e6c92287ef10dc7a09da28b8f28&language=en&query=";
-
-            switch (nextProps.selectValue) {
-                case 'All':
-                    type = "multi";
-                    break;
-                case 'Movie':
-                    type = "movie";
-                    break;
-                case 'TV Show':
-                    type = "tv";
-                    break;
-                case 'Actors':
-                    type = "person";
-                    break;
-                default:
-                    type = "multi";
-                    console.log('Error this should never happen :(');
-            }
-
-            var query = baseURL + type + options + nextProps.searchValue;
-
-            console.log('Searching: ' + query);
-            //Submit search query to The Movie Database servers
-            $.getJSON(query, function (data) {
-                this.setState({ loading: false, searchResults: data });
-            }.bind(this));
+        if (this.props.searchValue !== nextProps.searchValue || this.props.selectValue !== nextProps.selectValue) {
+            //If search value changed send movie api request and set loading to true.
+            this.setState({ loading: true, selectValue: nextProps.selectValue, searchQuery: nextProps.searchValue, currentPage: 1 });
         }
     },
     shouldComponentUpdate: function (nextProps, nextState) {
         //only update if searchQuery or loading value has changed.
-        return nextState.searchQuery !== this.state.searchQuery || nextState.loading !== this.state.loading;
+        return nextState.searchQuery !== this.state.searchQuery || nextState.selectValue !== this.state.selectValue || nextState.loading !== this.state.loading || nextState.currentPage !== this.state.currentPage;
     },
-    componentWillUpdate: function (nextProps, nextState) {},
+    componentWillUpdate: function (nextProps, nextState) {
+        //We only ever set loading when we are going to send a new API request
+        if (this.state.loading !== nextState.loading) {
+            this.apiQuery(nextState);
+        }
+    },
     render: function () {
         //Search bar is empty
         if (this.props.searchValue === "") {
@@ -151,10 +169,19 @@ var SearchResults = React.createClass({
                 if (this.state.searchResults.results.length > 0) {
                     return React.createElement(
                         'div',
-                        { className: 'card-columns' },
-                        this.state.searchResults.results.map(function (result) {
-                            return React.createElement(MovieCard, { key: result.id, data: result });
-                        })
+                        null,
+                        React.createElement(
+                            'div',
+                            { className: 'card-columns' },
+                            this.state.searchResults.results.map(function (result) {
+                                return React.createElement(MovieCard, { key: result.id, data: result });
+                            })
+                        ),
+                        React.createElement(
+                            'div',
+                            { style: { textAlign: 'center' } },
+                            React.createElement(Pageination, { currentPage: this.state.currentPage, totalPages: this.state.totalPages, handleClick: this.handlePageSelect })
+                        )
                     );
                 } else {
                     //No results
@@ -174,6 +201,65 @@ var SearchResults = React.createClass({
                     );
                 }
             }
+    }
+});
+
+//Gives ability to go through pages
+var Pageination = React.createClass({
+    displayName: 'Pageination',
+
+    render: function () {
+        //Setup pagination based on the currently view page.
+        //There is no easy way to do this without this fugly code.
+        var pages = [];
+        var start = this.props.currentPage <= 3 ? 1 : this.props.currentPage - 3;
+        var end = this.props.currentPage + 3 > this.props.totalPages ? this.props.totalPages : this.props.currentPage + 3;
+        for (var i = start; i <= end; i++) {
+            pages.push(React.createElement(
+                'li',
+                { onClick: this.props.handleClick.bind(null, i), key: i, className: i === this.props.currentPage ? "page-item active" : "page-item" },
+                React.createElement(
+                    'a',
+                    { className: 'page-link', href: i !== this.props.currentPage ? '#' : null },
+                    i
+                )
+            ));
+        }
+        return React.createElement(
+            'nav',
+            null,
+            React.createElement(
+                'ul',
+                { className: 'pagination' },
+                this.props.currentPage !== 1 ? React.createElement(
+                    'li',
+                    { className: 'page-item' },
+                    React.createElement(
+                        'a',
+                        { className: 'page-link', id: 'back', onClick: this.props.handleClick.bind(null, this.props.currentPage - 1), href: '#', 'aria-label': 'Previous' },
+                        React.createElement(
+                            'span',
+                            null,
+                            '«'
+                        )
+                    )
+                ) : null,
+                pages,
+                this.props.currentPage !== this.props.totalPages ? React.createElement(
+                    'li',
+                    { className: 'page-item' },
+                    React.createElement(
+                        'a',
+                        { className: 'page-link', id: 'forward', onClick: this.props.handleClick.bind(null, this.props.currentPage + 1), href: '#', 'aria-label': 'Next' },
+                        React.createElement(
+                            'span',
+                            null,
+                            '»'
+                        )
+                    )
+                ) : null
+            )
+        );
     }
 });
 
--- js/movieSearch.jsx
+++ js/movieSearch.jsx
@@ -1,6 +1,6 @@
 var SearchBar = React.createClass({
     getInitialState: function() {
-        return {inputValue: '', searchValue: '', selectValue: 'All'};
+        return {inputValue: '', searchValue: '', selectValue: 'multi'};
     },
     handleChange: function(event) {
         //Cut user input to max length of 140 characters.
@@ -8,12 +8,37 @@ var SearchBar = React.createClass({
         //This way we don't spam their servers every keystroke.
         this.setState({inputValue: event.target.value.substr(0, 140)});
         clearTimeout(this.searchTimeOut);
-        this.searchTimeOut = setTimeout(function(){ this.setState({searchValue: this.state.inputValue})}.bind(this), 300)
+        this.searchTimeOut = setTimeout(function(){
+            //only set the state if the input has changed and isn't empty.
+            if(this.state.inputValue !== this.state.searchValue && this.state.inputValue !== ""){
+                this.setState({searchValue: this.state.inputValue})
+            }
+        }.bind(this), 300)
     },
     handleSelect: function(event) {
         //Handles search bar dropdown menu.
         if(this.state.selectValue !== event.target.innerHTML){
-            this.setState({selectValue: event.target.innerHTML});
+            var type;
+
+            //Set prop to appropriate string for the search query later.
+            switch(event.target.innerHTML){
+                case 'All':
+                    type = "multi"
+                    break;
+                case 'Movie':
+                    type = "movie"
+                    break;
+                case 'TV Show':
+                    type = "tv"
+                    break;
+                case 'Actors':
+                    type = "person"
+                    break;
+                default:
+                    type = "multi"
+                    console.log('Error this should never happen :(');
+            }
+            this.setState({selectValue: type});
         }
     },
     searchTimeOut: setTimeout(function(){}),
@@ -46,53 +71,48 @@ var SearchBar = React.createClass({
 
 var SearchResults = React.createClass({
     getInitialState: function() {
-        return {searchResults: [], loading: false, searchQuery: "EMPTY"};
+        return {searchResults: [], loading: false, searchQuery: "EMPTY", selectValue: "EMPTY", totalPages: 0, currentPage: 1};
+    },
+    handlePageSelect: function(page, event){
+        //Handle pagination clicks and sets page to new one
+        if(page !== this.state.currentPage){
+            this.setState({currentPage: page, loading: true});
+            console.log('PAGE SELECTION NUMBER: ' + page);
+        }
+    },
+    apiQuery: function(nextState){
+        //Setup url for search
+        var baseURL = "http://api.themoviedb.org/3/search/";
+        var type = nextState.selectValue;
+        var options = "?api_key=1fa14e6c92287ef10dc7a09da28b8f28&language=en&query=";
+        var page = "&page=" + nextState.currentPage;
+
+        var query = baseURL + type + options + nextState.searchQuery + page;
+        console.log('search query: ' + query)
+        //Submit search query to The Movie Database servers
+        $.getJSON(query , function( data ) {
+            this.setState({loading: false, searchResults: data, totalPages: data.total_pages,  currentPage: data.page});
+        }.bind(this));
     },
     componentWillReceiveProps: function(nextProps) {
-        //If search value changed send movie api request and set loading to true.
-        if (nextProps.searchValue !== "" &&
-            (nextProps.searchValue !== this.props.searchValue ||
-            this.props.selectValue !== nextProps.selectValue)){
-            this.setState({loading: true, searchQuery: nextProps.searchValue});
-
-            //Setup url for search
-            var baseURL = "http://api.themoviedb.org/3/search/";
-            var type = "";
-            var options = "?api_key=1fa14e6c92287ef10dc7a09da28b8f28&language=en&query=";
-
-            switch(nextProps.selectValue){
-                case 'All':
-                    type = "multi"
-                    break;
-                case 'Movie':
-                    type = "movie"
-                    break;
-                case 'TV Show':
-                    type = "tv"
-                    break;
-                case 'Actors':
-                    type = "person"
-                    break;
-                default:
-                    type = "multi"
-                    console.log('Error this should never happen :(');
-            }
-
-            var query = baseURL + type + options + nextProps.searchValue;
-
-            console.log('Searching: ' + query);
-            //Submit search query to The Movie Database servers
-            $.getJSON(query , function( data ) {
-                this.setState({loading: false, searchResults: data});
-            }.bind(this));
+        if (this.props.searchValue !== nextProps.searchValue ||
+            this.props.selectValue !== nextProps.selectValue){
+            //If search value changed send movie api request and set loading to true.
+            this.setState({loading: true, selectValue: nextProps.selectValue, searchQuery: nextProps.searchValue, currentPage: 1});
         }
     },
     shouldComponentUpdate: function(nextProps, nextState) {
         //only update if searchQuery or loading value has changed.
         return  nextState.searchQuery !== this.state.searchQuery ||
-                nextState.loading !== this.state.loading;
+                nextState.selectValue !== this.state.selectValue ||
+                nextState.loading !== this.state.loading ||
+                nextState.currentPage !== this.state.currentPage;
     },
     componentWillUpdate: function(nextProps, nextState) {
+        //We only ever set loading when we are going to send a new API request
+        if(this.state.loading !== nextState.loading){
+            this.apiQuery(nextState);
+        }
     },
     render: function() {
         //Search bar is empty
@@ -115,10 +135,16 @@ var SearchResults = React.createClass({
             //Display search results
             if (this.state.searchResults.results.length > 0){
                 return (
-                    <div className="card-columns">
-                        {this.state.searchResults.results.map(function(result) {
-                            return <MovieCard key={result.id} data={result}/>;
-                        })}
+                    <div>
+                        <div className="card-columns">
+                            {this.state.searchResults.results.map(function(result) {
+                                return <MovieCard key={result.id} data={result}/>;
+                            })}
+
+                        </div>
+                        <div style={{textAlign: 'center'}}>
+                            <Pageination currentPage={this.state.currentPage} totalPages={this.state.totalPages} handleClick={this.handlePageSelect}/>
+                        </div>
                     </div>
                 );
             } else {
@@ -131,6 +157,42 @@ var SearchResults = React.createClass({
                 );
             }
         }
+    }
+});
+
+//Gives ability to go through pages
+var Pageination = React.createClass({
+    render: function() {
+        //Setup pagination based on the currently view page.
+        //There is no easy way to do this without this fugly code.
+        var pages = [];
+        var start = this.props.currentPage <= 3 ? 1 : this.props.currentPage - 3;
+        var end = this.props.currentPage + 3 > this.props.totalPages ? this.props.totalPages : this.props.currentPage + 3;
+        for (var i = start; i <= end; i++) {
+          pages.push(<li onClick={this.props.handleClick.bind(null, i)} key={i} className={i === this.props.currentPage ? "page-item active" : "page-item"}><a className="page-link" href={i !== this.props.currentPage ? '#' : null}>{i}</a></li>);
+        }
+        return (
+            <nav>
+                <ul className="pagination">
+                    {this.props.currentPage !== 1 ?
+                        <li className="page-item">
+                            <a className="page-link" id="back" onClick={this.props.handleClick.bind(null, this.props.currentPage - 1)} href='#' aria-label="Previous">
+                                <span>&laquo;</span>
+                            </a>
+                        </li> : null
+                    }
+                    {pages}
+                    {this.props.currentPage !== this.props.totalPages ?
+                        <li className="page-item">
+                            <a className="page-link" id="forward" onClick={this.props.handleClick.bind(null, this.props.currentPage + 1)} href='#' aria-label="Next">
+                                <span>&raquo;</span>
+                            </a>
+                        </li> : null
+                    }
+
+                </ul>
+            </nav>
+        );
     }
 });
 
